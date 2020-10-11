31661de2f86d8f9e70e275f7e49d3f25473e83f1
Elham-Ahmadzadeh <59868567+Elham-Ahmadzadeh@users.noreply.github.com>
2020-10-11 22:54:24 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,83 @@
+ let div = document.createElement('div')
+document.body.appendChild(div)
+div.setAttribute('id', 'bet-size')
+let bet = document.querySelector('#bet-size')
+
+
+let element = document.createElement('input')
+document.body.appendChild(element)
+element.setAttribute('type', 'button')
+element.id = 'increase'
+let increase = document.querySelector('#increase')
+
+let secElement = document.createElement('input')
+document.body.appendChild(secElement)
+secElement.setAttribute('type', 'button')
+secElement.id = 'decrease'
+let decrease = document.querySelector('#decrease')
+
+bet.textContent = 1
+ 
+
+increase.addEventListener('click', updateIncrease)
+
+function updateIncrease() {
+   
+    bet.textContent = Number(bet.textContent) + 1
+   
+    decrease.removeAttribute('disabled')
+   
+}
+ 
+
+decrease.addEventListener('click', updateDecrease)
+
+function controlKeys() {
+    document.body.addEventListener('keydown', event => {
+        switch (event.key) {
+            case 'ArrowUp':
+                console.log('up')
+                updateIncrease()
+                break
+            case 'ArrowDown':
+                if (Number(bet.textContent) === 1) {
+                       break
+        }
+                console.log('down')
+                updateDecrease()
+                break
+         
+        }
+        
+   
+    })
+}
+controlKeys()
+
+    if (Number(bet.textContent) === 1) {
+         decrease.setAttribute('disabled', 'disabled')
+}
+
+function updateDecrease() {
+   
+    bet.textContent = Number(bet.textContent) - 1 
+     if (Number(bet.textContent) === 1) {
+         decrease.setAttribute('disabled', 'disabled')
+} 
+    }
+   
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
